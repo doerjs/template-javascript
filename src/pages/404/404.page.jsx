6fa7f55dd8f5f61e3129b04bb268774459194f1e
guifu.wu <guifu.wu@dmall.com>
2024-06-22 12:04:58 +0800

    feat: 修改模版

--- src/pages/404/404.page.jsx
+++ src/pages/404/404.page.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import styles from './404.module.css'
 
 export default function NotFound() {
--- src/pages/index/Index.page.jsx
+++ src/pages/index/Index.page.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import styles from './Index.module.css'
 
 export default function Index() {
